2d2b2b3c1ce2f13498cd5486b620708b24a5ddf9
John-Dig <rohnfiggins@gmail.com>
2023-01-23 14:35:30 -0800

    .5 passed

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,25 @@ function Place(id, placeName, yearVisited, sideLocations, notes) {
   this.id = id;
   this.placeName = placeName;
   this.yearVisited = yearVisited;
-  this.sideLocations = [];
+  this.sideLocations = {};
   this.notes = notes;
+}
+
+//business logic for adding sideLocations -sL-)
+Place.prototype.addSideLocations = function(sL1, sL2, sL3) {
+  this.sideLocations[sL1] = sL1;
+  this.sideLocations[sL2] = sL2;
+  this.sideLocations[sL3] = sL3;
 }
 
 // entering places
 let boston = new Place ("0","Boston", "2014", "The Bay", "It was more humid than I thought it would be.");
 let longBeach = new Place ("1","Long Beach, CA","2015","The Pyramid","It was a nice trip");
+longBeach.addSideLocations("Joes tent", "Joes sidewalk", "Joe's suitcase");
 
-//adding  method
-Place.prototype.addSideLocation = function(sideLocation) {
-  this.sideLocations.push(sideLocation);
-}
-boston.addSideLocation("Your mom's house");
-boston.addSideLocation("Grandmas Condo");
-console.log(boston);
+console.log(longBeach);
 
 
-//adding  a method
-Place.prototype.nameNotes = function() {
-  return this.placeName + " " + this.notes;
-};
 
 
 
